d58938726a9cf2a96d5cffc36565f52ef81f02f4
scnprt <frdw@protonmail.com>
2021-09-21 13:11:23 -0300

    added the shift functions call to main

--- help.c
+++ help.c
@@ -9,15 +9,16 @@ void print_help(char *pname)
 
 	printf("Options:\n");
 	printf("\t-h\t\t\tPrint this help message.\n");
-	printf("\t-e\t\t\tEncrypt the message. One of -e or -d required.\n");
-	printf("\t-d\t\t\tDecrypt the message. One of -e or -d required.\n");
+	printf("\t-e\t\t\tEncrypt the message. Required to encrypt the message.\n");
+	printf("\t-d\t\t\tDecrypt the message. Required to decrypt the message.\n");
 	printf("\t-c <number>\t\tSelect the cipher's number. Required.\n");
+	printf("\t-k <number>\t\tSome ciphers require a key, that must be a positive number.\n");
 	printf("\t-x\t\t\tPrint the encrypted message as hexadecimal.\n");
 
 	printf("\n");
 
 	printf("Available Ciphers:\n");
-	printf("\t0\t\t\tShift.\n");
-	printf("\t1\t\t\tCaesar.\n");
-	printf("\t2\t\t\tROT-13.\n");
+	printf("\t1\t\t\tShift. Requires a key.\n");
+	printf("\t2\t\t\tCaesar.\n");
+	printf("\t3\t\t\tROT-13.\n");
 }
--- main.c
+++ main.c
@@ -8,19 +8,18 @@
 int main(int argc, char **argv)
 {
 	char *pname = argv[0];
-	unsigned int encode_flag = 0;
-	unsigned int decode_flag = 0;
-	char *cipher = NULL;
-	unsigned int key;
-	unsigned int hex_flag = 0;
+	uint8_t encode_flag = 0;
+	uint8_t decode_flag = 0;
+	uint8_t cipher;
+	uint8_t key = 0;
+	uint8_t hex_flag = 0;
 	char *message = NULL;
 	int c;
 
 	// set the argparser not to print any errors.
 	opterr = 0;
 		
-	// check if the number of arguments is zero and
-	// print out the help.
+	//check if the number of arguments is zero and print out the help.
 	if (argc == 1)
 	{
 		print_help(pname);
@@ -28,7 +27,7 @@ int main(int argc, char **argv)
 	}
 
 	// parse the CLI arguments.
-	while ((c = getopt(argc, argv, "hedc:kx")) != -1)
+	while ((c = getopt(argc, argv, ":hedxc:k:")) != -1)
 	{
 		switch(c)
 		{
@@ -51,7 +50,22 @@ int main(int argc, char **argv)
 				}
 				break;
 			case 'c':
-				cipher = optarg;
+				if (!atoi(optarg))
+				{
+					fprintf(stderr, "%s: cipher number not valid.\n", pname);
+					fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+					return EXIT_FAILURE;
+				}
+				cipher = atoi(optarg);
+				break;
+			case 'k':
+				if (atoi(optarg) <= 0)
+				{
+					fprintf(stderr, "%s: key value must be a potivive int.\n", pname);
+					fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+					return EXIT_FAILURE;
+				}
+				key = atoi(optarg);
 				break;
 			case 'x':
 				hex_flag = 1;
@@ -59,17 +73,13 @@ int main(int argc, char **argv)
 			case 'h':
 				print_help(pname);
 				return EXIT_SUCCESS;
+			case ':':
+				fprintf(stderr, "%s: option '-%c' requires an argument.\n", pname, optopt);
+				fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+				return EXIT_FAILURE;
 			case '?':
-				if (optopt == 'c')
-				{
-					fprintf(stderr, "%s: option '-%c' requires a cipher number as an argument.\n", pname, optopt);
-					fprintf(stderr, "Try '%s -h' for more information.\n", pname);
-				}
-				else if (isprint(optopt))
-				{
-					fprintf(stderr, "%s: invalid option '-%c'.\n", pname, optopt);
-					fprintf(stderr, "Try '%s -h' for more information.\n", pname);
-				}
+				fprintf(stderr, "%s: invalid option '-%c'.\n", pname, optopt);
+				fprintf(stderr, "Try '%s -h' for more information.\n", pname);
 				return EXIT_FAILURE;
 			default:
 				print_help(pname);
@@ -77,6 +87,7 @@ int main(int argc, char **argv)
 		}
 	}
 
+	// check if both -e and -d are set.
 	if (encode_flag == 0 && decode_flag == 0)
 	{
 		fprintf(stderr, "%s: one of -e of -d is required.\n", pname);
@@ -92,5 +103,68 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Try '%s -h' for more information.\n", pname);
 		return EXIT_FAILURE;
 	}
-	printf("Message: %s\n", message);
+
+	char *res = NULL;
+
+	// call the cipher function.
+	switch (cipher)
+	{
+		case 1: // Shift.
+			if (key == 0)
+			{
+				fprintf(stderr, "%s: shift cipher requires a key.\n", pname);
+				fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+				return EXIT_FAILURE;
+			}
+			if (encode_flag == 1)
+			{
+				res = enc_shift(message, key % 26); // cap the key value limit to 26 (alphabet letters)
+			}
+			else if (decode_flag == 1)
+			{
+				res = dec_shift(message, key % 26); // cap the key value limit to 26 (alphabet letters)
+			}
+			break;
+		case 2: // Caesar.
+			if (encode_flag == 1)
+			{
+				res = enc_shift(message, 3);
+			}
+			else if (decode_flag == 1)
+			{
+				res = dec_shift(message, 3);
+			}
+			break;
+		case 3: // ROT-13.
+			if (encode_flag == 1)
+			{
+				res = enc_shift(message, 13);
+			}
+			else if (decode_flag == 1)
+			{
+				res = dec_shift(message, 13);
+			}
+			break;
+		default:
+			fprintf(stderr, "%s: cipher number not valid.\n", pname);
+			fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+			return EXIT_FAILURE;
+	}
+
+	// print out the result.
+	if (res != NULL)
+	{
+		printf("Message: %s\n", message);
+		printf("Result: %s\n", res);
+		if (hex_flag == 1)
+		{
+			printf("Result in hexadecimal: 0x%x\n", res);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "%s: could not encrypt or decrypt the message.\n", pname);
+		fprintf(stderr, "Try '%s -h' for more information.\n", pname);
+		return EXIT_FAILURE;
+	}
 }
